402e74d74cf044787fdd395f16b7c9be132a8d40
andreaborghesi00 <00borghesiandrea@gmail.com>
2025-02-06 23:54:40 +0100

    added configuration file

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Dict, Any
 import yaml
 import os
@@ -8,10 +8,13 @@ import logging
 @dataclass(frozen=True)
 class ModelConfig:
     experiment_name: str
+    project_dir: str = field(default=os.path.join(Path(__file__).parent.parent.parent), init=False)
     
     # Model architecture
     image_input_channels: int = 1
     num_classes: int = 2
+    output_dim: int = 4
+    
     
     # Training hyperparameters
     batch_size: int = 32
@@ -20,10 +23,10 @@ class ModelConfig:
     dropout_rate: float = 0.3
 
     # Paths and Logging
-    data_path: str = "../../data/slices_segmentate_png"
-    annotation_path: str = "../../data/annotations.csv"
-    log_dir: str = "../../logs"
-    checkpoint_dir: str = "../../checkpoints"
+    data_path: str = "data/slices_segmentate_png"
+    annotation_path: str = "data/annotations.csv"
+    log_dir: str = "logs"
+    checkpoint_dir: str = "checkpoints"
     
     def validate(self):
         assert self.image_input_channels > 0, "Image input channels must be greater than 0"
@@ -38,25 +41,28 @@ class ModelConfig:
         assert self.checkpoint_dir, "Checkpoint directory must be specified"
     
     @classmethod
-    def load_config(cls, config_path: str = "config.yaml") -> "ModelConfig":
+    def load_config(cls, config_path: str = os.path.join(Path(__file__).parent, "config.yaml")) -> "ModelConfig":
         # log current working directory
+        if not os.path.exists(config_path):
+            raise FileNotFoundError(f"Config file not found at path {config_path}")
+        
         with open(config_path, "r") as file:
             config_dict = yaml.safe_load(file)
         return cls(**config_dict)
 
 # singleton
-_config_istance = None
+_config_instance = None
 
 def get_config(config_path: str = None):
-    global _config_istance
-    if _config_istance is None:
+    global _config_instance
+    if _config_instance is None:
         if config_path is None:
             config_path = os.path.join(Path(__file__).parent, "config.yaml") # default config path
-        _config_istance = ModelConfig.load_config(config_path)
+        _config_instance = ModelConfig.load_config(config_path)
     
-    return _config_istance
+    return _config_instance
 
 def reset_config():
-    global _config_istance
-    _config_istance = None
+    global _config_instance
+    _config_instance = None
         
