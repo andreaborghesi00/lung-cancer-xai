34e1e8b636edf6665d3da1c179424f27c1f7d04f
andreaborghesi00 <00borghesiandrea@gmail.com>
2025-02-06 23:55:45 +0100

    utils functions

---
+++ src/utils/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+import logging
+
+def roi_overlay(image, roi, color=(255, 0, 0), lw=2):
+    x, y, w, h = roi
+    image = image.copy()
+    image[y:y+h, x:x+lw] = color
+    image[y:y+h, x+w:x+w+lw] = color
+    image[y:y+lw, x:x+w] = color
+    image[y+h:y+h+lw, x:x+w] = color
+    return image
+
+
+def greyscale_to_rgb(image):
+    # return np.stack([image, image, image], axis=2)
+    return np.repeat(image[..., np.newaxis], 3, axis=-1)
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
