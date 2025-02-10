5b58f654112eacd4489048f28b327fb62151002f
andreaborghesi00 <00borghesiandrea@gmail.com>
2025-02-10 20:20:22 +0100

    switch to mobilenet and dynamic image loading dataset

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import logging
-from utils.utils import setup_logging
+import utils.utils as utils
 from config.config import get_config
 import torch
 from sklearn.model_selection import train_test_split
 from data.preprocessing import ROIPreprocessor
 from data.image_dataset import ImageDataset
-from data.rcnn_dataset import FasterRCNNDataset
+from data.rcnn_dataset import StaticRCNNDataset, DynamicRCNNDataset
 from models.roi_regressor import RoiRegressor
-from models.faster_rcnn import FasterRCNN
+import models.faster_rcnn as frcnn
 from training.trainer import ROITrainer
 from training.rcnn_trainer import RCNNTrainer
 import gc
@@ -16,12 +16,19 @@ if __name__ == "__main__":
     config = get_config()
     config.validate()
     logger = logging.getLogger(__name__)
-    setup_logging(level=logging.INFO)
+    utils.setup_logging(level=logging.INFO)
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    
+    # model 
+    logger.info("Initializing model")
+    model = frcnn.FasterRCNNMobileNet()
+    logger.info(f"Model initialized {model.__class__.__name__} with {utils.count_parameters(model)} trainable parameters")
+    model = model.to(device) # maybe this will keep it off the ram
     
     # prepare data
     preprocessor = ROIPreprocessor()
-    data, labels = preprocessor.load_data_labels()
+    data, labels = preprocessor.load_paths_labels() # no transformation here, i do not have enough ram apparently
 
     # split data
     X_train, X_valtest, y_train, y_valtest = train_test_split(data, labels, test_size=0.3, random_state=0)
@@ -32,8 +39,8 @@ if __name__ == "__main__":
     
     # datasets and dataloaders
     logger.info("Creating datasets and dataloaders")
-    train_ds = FasterRCNNDataset(X_train, y_train)
-    val_ds = FasterRCNNDataset(X_val, y_val)
+    train_ds = DynamicRCNNDataset(X_train, y_train, transform=model.get_transform())
+    val_ds = DynamicRCNNDataset(X_val, y_val, transform=model.get_transform())
     
     logger.info("Creating dataloaders")
     train_dl = train_ds.get_loader(shuffle=True)
@@ -43,14 +50,11 @@ if __name__ == "__main__":
     del X_train, X_val, y_train, y_val, X_valtest, y_valtest, train_ds, val_ds, data, labels
     gc.collect() # garbage collection
     
-    # model definition
-    logger.info("Creating the model")
-    model = FasterRCNN()
-      
+
     # optimizer and scheduler
     logger.info("Initializing optimizer and scheduler")
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=4) # max since we are maximizing iou
     
     # training
     logger.info("Initializing trainer")
@@ -74,7 +78,7 @@ if __name__ == "__main__":
     
     # testing
     logger.info("Testing the model")
-    test_ds = FasterRCNNDataset(X_test, y_test)
+    test_ds = DynamicRCNNDataset(X_test, y_test, transform=model.get_transform())
     test_dl = test_ds.get_loader()
     test_metrics = trainer.validation(test_dl) 
     
