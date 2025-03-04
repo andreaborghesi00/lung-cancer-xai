816aea1036de8533ac0915410a5882f623b4edde
andreaborghesi00 <00borghesiandrea@gmail.com>
2025-03-04 15:38:31 +0100

    tomography aware testing

--- src/explain.py
+++ src/explain.py
@@ -2,6 +2,8 @@ import logging
 import random
 from config.config import get_config
 from data.rcnn_dataset import DynamicRCNNDataset
+from data.tomography_dataset import DynamicTomographyDataset
+from training.rcnn_trainer import RCNNTrainer
 import utils.utils as utils
 from models.faster_rcnn import FasterRCNNMobileNet, FasterRCNNResnet50
 from explainers.grad_cam import CAMExplainer
@@ -14,13 +16,14 @@ from sklearn.model_selection import train_test_split
 import gc
 from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
 import numpy as np
+
 if __name__ == "__main__":
     config = get_config()
     config.validate()
     logger = logging.getLogger(__name__)
     utils.setup_logging(level=logging.INFO)
     
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     #load model
     model_path = Path(config.checkpoint_dir) / config.model_checkpoint
@@ -36,59 +39,78 @@ if __name__ == "__main__":
     X_test, y_test = preprocessor.load_paths_labels(test_ids)
     test_ds = DynamicRCNNDataset(X_test, y_test, transform=model.get_transform())
     test_dl = test_ds.get_loader()
+    test_ds_tomo = DynamicTomographyDataset(test_ids, transform=model.get_transform())
+    test_dl_tomo = test_ds_tomo.get_loader(batch_size=1)
     
     del valtest_ids, test_ids, unique_tomographies
     gc.collect()
     
-    visualizer = Visualizer()    
+    # test model
+    trainer = RCNNTrainer(model=model,
+                            optimizer=None,
+                            scheduler=None,
+                            device=device,
+                            checkpoint_dir=config.checkpoint_dir,
+                            use_wandb=config.use_wandb
+                          )
+    
+    logger.info("Testing model - Non tomography aware")
+    metrics = trainer.validation(test_dl)
+    logger.info(f"Test metrics: {metrics}")
+    
+    logger.info("Testing model - Tomography aware")
+    metrics = trainer.tomography_aware_validation(test_dl_tomo)
+    logger.info(f"Test metrics: {metrics}")
+    
+    # visualizer = Visualizer()    
 
-    num_samples = len(test_ds) # all of them
-    # sample_indices = random.sample(range(len(test_ds)), num_samples)
+    # num_samples = len(test_ds) # all of them
+    # # sample_indices = random.sample(range(len(test_ds)), num_samples)
     
-    logger.info(f"Generating visualizations for {num_samples} random test samples")
-    model.eval()
+    # logger.info(f"Generating visualizations for {num_samples} random test samples")
+    # model.eval()
     
-    logger.info(model.model.backbone)
-    inverse_layer_nums = [1]
-    target_layers = [[list(model.model.backbone.body.children())[-(i+1)]] for i in inverse_layer_nums] # get last layer for RCNN model, unfortunately this is model specific
-    # apply the explainer to the test samples
-    explainers = [CAMExplainer(model=model, target_layer=target_layer, target_class=1, cam_class=GradCAM) for target_layer in target_layers]
-    logger.info(f"Explainers initialized, length: {len(explainers)}")
-    for idx in tqdm(range(num_samples)):
-        image_path = X_test[idx]
-        image, target = test_ds[idx]
-        image = image.unsqueeze(0).to(device)
-        image.requires_grad = True
+    # logger.info(model.model.backbone)
+    # inverse_layer_nums = [1]
+    # target_layers = [[list(model.model.backbone.body.children())[-(i+1)]] for i in inverse_layer_nums] # get last layer for RCNN model, unfortunately this is model specific
+    # # apply the explainer to the test samples
+    # explainers = [CAMExplainer(model=model, target_layer=target_layer, target_class=1, cam_class=GradCAM) for target_layer in target_layers]
+    # logger.info(f"Explainers initialized, length: {len(explainers)}")
+    # for idx in tqdm(range(num_samples)):
+    #     image_path = X_test[idx]
+    #     image, target = test_ds[idx]
+    #     image = image.unsqueeze(0).to(device)
+    #     image.requires_grad = True
         
-        # get prediction to display predicted boxes
-        with torch.no_grad():
-            prediction = model(image)[0]
+    #     # get prediction to display predicted boxes
+    #     with torch.no_grad():
+    #         prediction = model(image)[0]
         
-        # filter predictions by confidence
-        confidence_threshold = 0.5
-        mask = prediction['scores'] > confidence_threshold
-        pred_boxes = prediction['boxes'][mask]
-        scores = prediction['scores'][mask]        
+    #     # filter predictions by confidence
+    #     confidence_threshold = 0.5
+    #     mask = prediction['scores'] > confidence_threshold
+    #     pred_boxes = prediction['boxes'][mask]
+    #     scores = prediction['scores'][mask]        
         
-        true_boxes = target['boxes'] # ground truth boxes
+    #     true_boxes = target['boxes'] # ground truth boxes
         
-        image_numpy = image.detach().squeeze(0).cpu().numpy().transpose(1, 2, 0) # convert to numpy and convert from (C, H, W) to (H, W, C)
-        for layer_idx, target_layer in enumerate(target_layers):
-            explainer = explainers[layer_idx]
-            try:
-                grayscale_cam = explainer.explain(image=image, labels=[1], bboxes=[target['boxes'][0].cpu().numpy()])
-            except Exception as e:
-                logger.error(f"Failed to generate CAM for sample {idx} at layer {layer_idx}: {str(e)}")
-                continue
+    #     image_numpy = image.detach().squeeze(0).cpu().numpy().transpose(1, 2, 0) # convert to numpy and convert from (C, H, W) to (H, W, C)
+    #     for layer_idx, target_layer in enumerate(target_layers):
+    #         explainer = explainers[layer_idx]
+    #         try:
+    #             grayscale_cam = explainer.explain(image=image, labels=[1], bboxes=[target['boxes'][0].cpu().numpy()])
+    #         except Exception as e:
+    #             logger.error(f"Failed to generate CAM for sample {idx} at layer {layer_idx}: {str(e)}")
+    #             continue
                 
-            visualization = explainer.visualize(image=image_numpy, cam=grayscale_cam[0])
-            visualizer.display_bboxes(
-                input=visualization,
-                pred_boxes=pred_boxes,
-                true_boxes=true_boxes,
-                scores=scores,
-                filename=f"sample_{idx}_L{layer_idx}.png"
-            )
+    #         visualization = explainer.visualize(image=image_numpy, cam=grayscale_cam[0])
+    #         visualizer.display_bboxes(
+    #             input=visualization,
+    #             pred_boxes=pred_boxes,
+    #             true_boxes=true_boxes,
+    #             scores=scores,
+    #             filename=f"sample_{idx}_L{layer_idx}.png"
+    #         )
     
         # with torch.no_grad():
     #     for idx in tqdm(sample_indices):
--- src/train_od.py
+++ src/train_od.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     config.validate()
     logger = logging.getLogger(__name__)
     utils.setup_logging(level=logging.INFO)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")    
     
     # model 
     logger.info("Initializing model")
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     logger.info(f"Model initialized {model.__class__.__name__} with {utils.count_parameters(model)} trainable parameters")
     
     
-    if torch.cuda.device_count() > 1:
-        logger.info(f"Using {torch.cuda.device_count()} GPUs for training")
-        model = torch.nn.DataParallel(model, [0, 1])  # Multi-GPU support
+    # if torch.cuda.device_count() > 1:
+    #     logger.info(f"Using {torch.cuda.device_count()} GPUs for training")
+    #     model = torch.nn.DataParallel(model, [0, 1])  # Multi-GPU support
 
     model = model.to(device)
     logger.debug(model.module if isinstance(model, torch.nn.DataParallel) else model)
@@ -53,12 +53,12 @@ if __name__ == "__main__":
     logger.info("Creating datasets and dataloaders")
     transform = model.module.get_transform() if isinstance(model, torch.nn.DataParallel) else model.get_transform()
     train_ds = DynamicRCNNDataset(X_train, y_train, transform=transform)
-    # val_ds = DynamicRCNNDataset(X_val, y_val, transform=transform)
-    val_ds = DynamicTomographyDataset(val_ids, transform=transform)
+    val_ds = DynamicRCNNDataset(X_val, y_val, transform=transform)
+    # val_ds = DynamicTomographyDataset(val_ids, transform=transform)
     
     logger.info("Creating dataloaders")
-    train_dl = train_ds.get_loader(shuffle=True, batch_size=config.batch_size * torch.cuda.device_count())
-    val_dl = val_ds.get_loader(batch_size = 1) # this loader gets whole tomographies, hence the smaller batch size
+    train_dl = train_ds.get_loader(shuffle=True, batch_size=config.batch_size)
+    val_dl = val_ds.get_loader(batch_size = config.batch_size) # this loader gets whole tomographies, hence the smaller batch size
   
     # free memory
     del X_train, X_val, y_train, y_val, train_ds, val_ds, unique_tomographies, train_ids, val_ids
@@ -75,15 +75,18 @@ if __name__ == "__main__":
         model=model,
         optimizer=optimizer,
         scheduler=scheduler,
-        train_loader=train_dl,
-        val_loader=val_dl,
+
         device=device,
         checkpoint_dir=config.checkpoint_dir,
         use_wandb=config.use_wandb
     )
     
     logger.info("Training the model")
-    trainer.train(num_epochs=config.epochs)
+    trainer.train(num_epochs=config.epochs,
+                  patience=config.patience,
+                  train_loader=train_dl,
+                  val_loader=val_dl,
+                  )
     
     # free memory
     del train_dl, val_dl
@@ -95,5 +98,9 @@ if __name__ == "__main__":
     test_ds = DynamicRCNNDataset(X_test, y_test, transform=model.get_transform())
     test_dl = test_ds.get_loader()
     test_metrics = trainer.validation(test_dl) 
-    
     logger.info(f"Test metrics: {test_metrics}")
+    
+    test_ds_tomography = DynamicTomographyDataset(test_ids, transform=model.get_transform())
+    test_dl_tomography = test_ds_tomography.get_loader()
+    test_metrics_tomography = trainer.tomography_aware_validation(test_dl_tomography)
+    logger.info(f"Test metrics tomography: {test_metrics_tomography}")
